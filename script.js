7e4779dad649b321181f2e23c5ab346ec5c28436
NINOEY <drnoey782@gmail.com>
2025-07-11 21:58:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ for (let i = 0; i < numStars; i++) {
   star.style.animationDuration = `${1 + Math.random() * 2}s`;
   document.body.appendChild(star);
 }
+for (let i = 0; i < count; i++) {
+  const angle = i * (2 * Math.PI / count);
+  const x = 160 * Math.pow(Math.sin(angle), 3);
+  const y = -130 * Math.cos(angle) + 50 * Math.cos(2 * angle) + 20 * Math.cos(3 * angle) + 10 * Math.cos(4 * angle);
+
+  const span = document.createElement('span');
+  span.innerText = text;
+  span.style.left = `calc(50% + ${x}px)`;
+  span.style.top = `calc(50% + ${y}px)`;
+  heart
